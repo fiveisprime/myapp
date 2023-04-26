4bc52ee3d1327d34199722b427041e310da68314
Matt Hernandez <maherna@microsoft.com>
2023-04-26 15:00:29 -0700

    Verify runtime version

--- api/HttpTrigger1/index.js
+++ api/HttpTrigger1/index.js
@@ -1,6 +1,7 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
+    context.
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
@@ -8,6 +9,6 @@ module.exports = async function (context, req) {
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: process.env
     };
 }
